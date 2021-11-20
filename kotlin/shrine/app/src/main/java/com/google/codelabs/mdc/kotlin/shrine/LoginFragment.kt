04259f43c141e0cc1288aa2351a9481eb8e7ea3d
Godday Okoduwa <goddayokos@gmail.com>
2021-11-20 23:22:55 +0100

    Completed MDC 101

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -14,12 +17,34 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
 
         // Snippet from "Navigate to the next Fragment" section goes here.
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error
+                password_text_input.error = null
+                // Navigate to the next Fragment
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
 
-        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+        // Clear the error once more than * characters are typed
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text!!)) {
+                // Clear the error
+                password_text_input.error = null
+            } else {
+                password_text_input.error = getString(R.string.shr_error_password)
+            }
+            false
+        }
+
         return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?) = text != null && text.length >= 8
 }
