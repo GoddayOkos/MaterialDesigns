797742d8b670f2277d0b8744b14f69df04a1a335
Godday Okoduwa <goddayokos@gmail.com>
2021-11-21 13:26:51 +0100

    Completed MDC-104

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,8 +1,11 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,6 +27,11 @@ class ProductGridFragment : Fragment() {
 
         // Set up the toolbar
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(
+            activity!!, view.product_grid, AccelerateDecelerateInterpolator(),
+            ContextCompat.getDrawable(context!!, R.drawable.shr_branded_menu),
+            ContextCompat.getDrawable(context!!, R.drawable.shr_close_menu)
+        ))
 
         // Set up the RecyclerView
         view.recycler_view.setHasFixedSize(true)
@@ -40,6 +48,11 @@ class ProductGridFragment : Fragment() {
         val largePadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_large)
         val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_small)
         view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.product_grid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
 
         return view
     }
